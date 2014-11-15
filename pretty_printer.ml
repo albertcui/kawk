652441ad2c678f5bff4a4892269dda9c4985ba3d
Albert Cui <aqc2109@columbia.edu>
2014-11-15 03:29:42 -0500

    fix array printing

--- pretty_printer.ml
+++ pretty_printer.ml
@@ -54,8 +54,8 @@ let rec print_var_types = function
 let rec print_var_decl = function
 	Variable(var_types, str) -> print_var_types var_types; print_string (str ^ ";\n")
 	| Variable_Initialization(var_types, str, expr) -> print_var_types var_types; Printf.printf "%s = " str; print_expr_semi expr
-	| Array_Initialization(var_types, str, stmt) -> print_var_types var_types; Printf.printf "%s = " str; print_stmt stmt
-	| Struct_Initialization(var_types, str2, expr_list) -> print_var_types var_types; Printf.printf "%s = { " str2; List.iter print_expr expr_list; print_string "};\n"
+	| Array_Initialization(var_types, str, expr_list) -> print_var_types var_types; Printf.printf "%s = { " str; List.iter print_expr expr_list; print_string "};\n"
+	| Struct_Initialization(var_types, str, expr_list) -> print_var_types var_types; Printf.printf "%s = { " str; List.iter print_expr expr_list; print_string "};\n"
 
 
 let print_struct_body = function
