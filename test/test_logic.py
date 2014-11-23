f86c31767394f82b4cf2f9637a40f479442fee54
karenkn <karen.k.nan@gmail.com>
2014-11-23 03:53:26 -0500

    Prints output if rejected

--- test/test_logic.py
+++ test/test_logic.py
@@ -1,19 +1,36 @@
+#A simple test script to test for equality of 
+#the output after parsed by the trees and printed by the pretty printed
+#This will be expanded to accomodate more tests in the future
+
 import sys
 inputFileName = sys.argv[1]
 outputFileName = sys.argv[2]
 
-def convert_str(fileName):
-	file = open(fileName, 'r')
-	outputStr = file.read()
-	testStr = ''.join(outputStr.split())
+def convert_str(thestring):
+	testStr = ''.join(thestring.split())
 	return testStr
 
-inputFile = convert_str(inputFileName)
-outputFile = convert_str(outputFileName)
+#read the files
+file = open(inputFileName, 'r')
+inputFileStr = file.read()
+file = open(outputFileName, 'r')
+outputFileStr = file.read()
 
+#convert to string
+inString = str(inputFileStr)
+outString = str(outputFileStr)
+
+#convert the string by taking out spaces, newlines, and tabs
+inputTestStr = convert_str(inString)
+outputTestStr = convert_str(outString)
 print inputFileName
 print outputFileName
-if (outputFile == inputFile):
+if (outputTestStr == inputTestStr):
 	print "ACCEPT\n"
 else:
-	print "REJECT\n"
+	print "REJECT "
+	print "The input file:"
+	print inputFileStr
+	print "The output file:"
+	print outputFileStr
+	print "\n"
