3bc289708838d32bb9ffdcdb29a0dd2e43118a88
karenkn <karen.k.nan@gmail.com>
2014-12-11 02:33:44 -0500

    added REJECT header to input for test scripts

--- test/test_logic.py
+++ test/test_logic.py
@@ -1,11 +1,16 @@
 #A simple test script to test for equality of 
-#the output after parsed by the trees and printed by the pretty printed
+#the output after parsed by the trees and printed by the pretty printer
 #This will be expanded to accomodate more tests in the future
+
+#Author: Karen Nan 
 
 import sys
 inputFileName = sys.argv[1]
 outputFileName = sys.argv[2]
+#flag that 
+shouldReject = False
 
+#convert the string by taking out spaces, newlines, and tabs
 def convert_str(thestring):
 	testStr = ''.join(thestring.split())
 	return testStr
@@ -13,19 +18,28 @@ def convert_str(thestring):
 #read the files
 file = open(inputFileName, 'r')
 inputFileStr = file.read()
-file = open(outputFileName, 'r')
+file = open(outputFileName, 'a+')
+file.write(" ")
 outputFileStr = file.read()
 
 #convert to string
 inString = str(inputFileStr)
 outString = str(outputFileStr)
 
-#convert the string by taking out spaces, newlines, and tabs
+
 inputTestStr = convert_str(inString)
 outputTestStr = convert_str(outString)
+
+
+print outputTestStr
+
+# if the test should 'reject', the overall test will   
+if (inputTestStr[:6] == "REJECT"):
+	shouldReject = True
+
 print inputFileName
 print outputFileName
-if (outputTestStr == inputTestStr):
+if (outputTestStr == inputTestStr and shouldReject == False):
 	print "ACCEPT\n"
 else:
 	print "REJECT "
