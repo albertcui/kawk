6ab80f4e9c6f6417d5fd3fb265285f5b8e689e8d
Albert Cui <aqc2109@columbia.edu>
2014-12-11 03:41:29 -0500

    fix sast definition

--- sast.mli
+++ sast.mli
@@ -1,5 +1,29 @@
 open Ast
 
+(* type var_types =
+	Void
+	| Int
+	| String
+	| Boolean
+	| Struct of string
+	| Array of var_types * expr
+
+type var_decl =
+	Variable of var_types * string
+	| Variable_Initialization of var_types * string * expr
+	| Array_Initialization of var_types * string * expr list
+	| Struct_Initialization of var_types * string * expr list *)
+
+type variable_decl = var_decl * var_types
+
+type function_decl = {
+	ftype: var_types;
+	fname : string; (* Name of the function *)
+	formals : variable_decl list; (* Formal argument names *)
+	locals : variable_decl list; (* Locally defined variables *)
+	body : stmt list;
+}
+
 type expr_detail =
 	Noexpr
 	| This
@@ -14,8 +38,7 @@ type expr_detail =
 	| Uniop of op * expression
 	| Binop of expr_detail * op * expression
 	| Assign of variable_decl * expression
-
-type expression = expr_detail * var_types
+and expression = expr_detail * var_types
 
 type stmt = 
 	Block of stmt list (* { ... } *)
@@ -25,14 +48,4 @@ type stmt =
 	| For of expression * expression * expression * stmt (* for (i=0;i<10;i=i+1) { ... } *)
 	| While of expression * stmt
 
-type variable_decl = var_decl * var_types
-
-type func_decl = {
-	ftype: var_types;
-	fname : string; (* Name of the function *)
-	formals : var_decl list; (* Formal argument names *)
-	locals : var_decl list; (* Locally defined variables *)
-	body : stmt list;
-}
-
-type program = struct_decl list * variable_decl list * func_decl list
+type program = struct_decl list * variable_decl list * function_decl list
