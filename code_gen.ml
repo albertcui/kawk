6488935686cc68541b56e17925d62fbc86d9b6f8
Mei-Vern Then <mt2837@columbia.edu>
2014-12-15 17:51:16 -0500

    everything except arrays, structs, and units

--- code_gen.ml
+++ code_gen.ml
@@ -22,11 +22,12 @@ let print_op = function
 	| And -> print_string "&& "
 	| Not -> print_string "! " 
 
-let rec print_expr = function
-(* 	Noexpr -> print_string ""
+(* FIX ID *)
+let rec print_expr (e : Sast.expr_detail) = match e with
+	Noexpr -> print_string ""
 	| This -> print_string "this "
-	| Null -> print_string "null " *)
-	 (* Id(id) -> Printf.printf "%s " id *)
+	| Null -> print_string "null "
+	(* | Id(id) -> print_var_decl id *)
 	| IntConst(i) -> Printf.printf "%d " i
 	| StrConst(str) -> Printf.printf "%s " str
 	| BoolConst(b) -> Printf.printf "%B " b
@@ -50,46 +51,34 @@ let rec print_expr_list_comma (el : Sast.expression list) = match el with
 	| hd::[] -> let (expr_detail, _) = hd in print_expr expr_detail
 	| hd::tl -> let (expr_detail, _) = hd in print_expr expr_detail; print_string ", "; print_expr_list_comma tl 
 
-(* and print_expr_comma expr =
-	print_expr expr; print_string ", "
-	 *)
 let print_expr_semi (e : Sast.expression) = 
 	let (expr_detail, _) = e in 
 	print_expr expr_detail; print_string ";\n"
 
-(* let rec print_expr_list (el : Sast.expression list) = match el with
-	(* List.iter (fun s -> let (s, _) = match s with  *)
-	[] -> print_string ""
-	| hd::[] -> let (expr_detail, _) = hd in print_expr el
-	| hd::tl -> let (expr_detail, _) = hd in print_expr el; print_string "; "; print_expr_list tl 
-	(* ) *) *)
-
 let rec print_stmt = function
-	(* Block(stmt_list) -> print_string "{"; List.iter print_stmt stmt_list; print_string "}\n" *)
-	Expr(expr) -> print_expr_semi expr
-	(* | Return(expr) -> print_string "return "; print_expr_semi expr
+	Block(stmt_list) -> print_string "{"; List.iter print_stmt stmt_list; print_string "}\n"
+	| Expr(expr) -> print_expr_semi expr
+	| Return(expr) -> print_string "return "; print_expr_semi expr
 	| If(expr, stmt1, stmt2) -> print_string "if ("; print_expr_semi expr; print_string ")"; print_stmt stmt1; print_stmt stmt2
-	| For(expr1, expr2, expr3, stmt) -> print_string "for ("; print_expr_semi expr1; print_string ";"; print_expr_semi expr2; print_string ";"; print_expr expr3; print_stmt stmt 
+	| For(expr1, expr2, expr3, stmt) -> print_string "for ("; print_expr_semi expr1; print_expr_semi expr2; let (expr_detail, _) = expr3 in print_expr expr_detail; print_stmt stmt 
 	| While(expr, stmt) -> print_string "while ("; print_expr_semi expr; print_string ")"; print_stmt stmt
- *)
-	| _ -> print_string ""
 
 (* CHANGE: CANNOT DECLARE VARIABLE AS VOID *)
+(* DID NOT CHECK IF ARRAY(VAR_TYPES, EXPR) WHERE EXPR IS INT
+   JAST should redecine var_types so that struct of struct_decl and array expr of expr_detail *)
 let rec print_var_types = function
 	Void -> print_string "void "
 	| Int -> print_string "int "
 	| String -> print_string "String " 
 	| Boolean -> print_string "boolean "
-(* 	| Struct(str) -> Printf.printf "public class %s " str 
-	| Array(var_types, expr) -> print_var_types var_types; print_string "["; print_expr expr; print_string "] " *)
+	(* | Struct(str) -> Printf.printf "public class %s " str  *)
+	(* | Array(var_types, expr) -> print_var_types var_types; print_string "["; print_expr expr; print_string "] " *)
  
-	| _ -> print_string ""
-
 let rec print_var_decl  (v : Sast.variable_decl) =
 	let (var_types, _) = v in match var_types with
-	Variable(var_types, str) -> print_var_types var_types; print_string (str ^ ";\n")
-	| Variable_Initialization(var_types, str, expr) -> print_var_types var_types; Printf.printf "%s = " str; print_expr_semi expr
-	| Array_Initialization(var_types, str, expr_list) -> print_var_types var_types; Printf.printf "[] %s = { " str; print_expr_list_comma expr_list; print_string "};\n"
+		Variable(var_types, str) -> print_var_types var_types; print_string (str ^ ";\n")
+		| Variable_Initialization(var_types, str, expr) -> print_var_types var_types; Printf.printf "%s = " str; print_expr_semi expr
+		| Array_Initialization(var_types, str, expr_list) -> print_var_types var_types; Printf.printf "[] %s = { " str; print_expr_list_comma expr_list; print_string "};\n"
 	(* | Struct_Initialization(var_types, str, expr_list) -> print_var_types var_types; Printf.printf "%s = { " str; print_expr expr_list; print_string "};\n" *)
 
 (* let print_assert_name expr = *)
@@ -167,10 +156,12 @@ let rec print_var_decl  (v : Sast.variable_decl) =
 	| Outer_udecl(str, udecl_params, udecl_check_val, false) -> print_string "unit:"; print_string (str ^ "(");  print_expr_list_comma udecl_params; print_string "):equals("; print_expr udecl_check_val; print_string "):reject;\n"
  *)
 
-let print_param = function
-	Param(var_types, str) -> print_var_types var_types; print_string (str)
+let print_param v =
+	let (var_types, _) = v in match var_types with
+	Variable(var_types, str) -> print_var_types var_types; print_string str
+	| _ -> raise (Failure "This should never happen")
 
-let rec print_param_list = function
+let rec print_param_list (p : Sast.variable_decl list) = match p with
 	[] -> print_string "";
 	| hd::[] -> print_param hd;
 	| hd::tl -> print_param hd; print_string ", "; print_param_list tl
@@ -182,22 +173,21 @@ let print_func_decl (f : Sast.function_decl) =
 		List.iter print_stmt f.checked_body;
 		print_string "}")
 	else
-		print_string ""
-		(* print_var_types f.ftype;
+		print_var_types f.ftype;
 		print_string f.fname; 
 		print_string "(";
 		print_param_list f.checked_formals; 
 		print_string ") {\n";
-		List.iter print_var_decl f.check_locals; 
+		List.iter print_var_decl f.checked_locals; 
 		List.iter print_stmt f.checked_body;
-		List.iter print_unit_decl f.checked_units;
-		print_string "}\n" *)
+		(* List.iter print_unit_decl f.ch0ecked_units; *)
+		print_string "}\n"
 
 let code_gen j =
 	let _ = print_string "public class Program {\n" in
 	let (structs, vars, funcs, unts) = j in
 			(* List.iter print_struct_decl structs; *)
-			(* List.iter print_var_decl vars; *)
+			List.iter print_var_decl vars;
 			List.iter print_func_decl (List.rev funcs);
 			(* List.iter print_unit_decl unts; *)
 			print_string "\n}"
